3c4290f0a60cb025530a311d623cada07c0cfd1f
GG <gg@Zions-MBP.lan>
2022-06-13 01:34:29 +0300

    A

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 const App = () => {
   return (
-    <div className="font-bold text-4xl m-[2em] text-red-600 underline">
-      Hello World
+    <div>
+      <div className="w-full fixed top-0 p-4 border-b-2 bg-slate-600">header</div>
+      <div className="mt-14"></div>
+      <div className="flex flex-col">
+        <div className="p-8 flex flex-dir gap-4">
+          <div>first Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, alias dicta minus at voluptate sapiente. Eaque amet velit deserunt tempora ullam earum, quis molestias quaerat saepe nesciunt, voluptas praesentium sit!</div>
+          <div>second Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, alias dicta minus at voluptate sapiente. Eaque amet velit deserunt tempora ullam earum, quis molestias quaerat saepe nesciunt, voluptas praesentium sit!</div>
+        </div>
+        <div className="p-8 flex flex-dir gap-4">
+          <div>first Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, alias dicta minus at voluptate sapiente. Eaque amet velit deserunt tempora ullam earum, quis molestias quaerat saepe nesciunt, voluptas praesentium sit!</div>
+          <div>second Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, alias dicta minus at voluptate sapiente. Eaque amet velit deserunt tempora ullam earum, quis molestias quaerat saepe nesciunt, voluptas praesentium sit!</div>
+        </div>
+      </div>
     </div>
   );
 };
